f7e4cb7d901c5ea66c1e7236b64781d65f98ff15
Stuart Rivero <stuart.rivero.consultant@springernature.com>
2023-09-14 09:58:56 +0100

    Initial project set up with HTTP API

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+val prometeus_version: String by project
+plugins {
+    kotlin("jvm") version "1.9.10"
+    id("io.ktor.plugin") version "2.3.4"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-server-openapi")
+    implementation("io.ktor:ktor-server-call-logging-jvm")
+    implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
